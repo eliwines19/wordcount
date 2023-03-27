cc2e3f7f57764d70df83a10e83fb40c6b9ee8d3a
Elijah Wines <wines.elijah@gmail.com>
2023-03-27 19:45:24 -0400

    created form that redirects to /count

--- wordcount/views.py
+++ wordcount/views.py
@@ -2,4 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html', {"title":"Word Count"})
+    return render(request, 'home.html')
+
+def count(request):
+    return render(request, 'count.html')
