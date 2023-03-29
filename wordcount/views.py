034b2fb529034556f73496e1336dd5b5dae60733
Elijah Wines <wines.elijah@gmail.com>
2023-03-28 20:19:14 -0400

    gathered different info on fulltext and created home link on count url

--- wordcount/views.py
+++ wordcount/views.py
@@ -12,12 +12,29 @@ def count(request):
     count = len(fulltext.split())
 
     # get the word that appeared the most
-    # wordcount = {}
 
-    # for i in fulltext.split():
-    #     print(i)
+    # first count the number of occurrences of each for
+    wordcount = {}
+    for i in fulltext.split():
+        if i in wordcount:
+            wordcount[i] += 1
+        else:
+            wordcount[i] = 1
+
+    # get the word with the highest number of occurrences
+    highest_word = ""
+    highest_count = 0
+    for word in wordcount:
+        if wordcount[word] > highest_count:
+            highest_word = word
+            highest_count = wordcount[word]
+    
+    print(highest_word)
+    
 
     return render(request, 'count.html', {
         "count":count,
-        "fulltext":fulltext
+        "fulltext":fulltext,
+        "highest_word":highest_word,
+        "wordcount":wordcount.items()
         })
