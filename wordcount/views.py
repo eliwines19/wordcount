331401fd8a35cf336125f41667f7856fe608cca5
Elijah Wines <wines.elijah@gmail.com>
2023-03-28 20:31:10 -0400

    added about page to test my knowledge of django control flow

--- wordcount/views.py
+++ wordcount/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
+def about(request):
+    return render(request, "about.html")
+
 def count(request):
     # gets the parameters
     fulltext = request.GET['fulltext']
